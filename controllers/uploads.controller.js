b9bd92a2d77bac0ea0afc63deb681caa01b7486e
Santiago Zapata <dev.santizapata@gmail.com>
2026-03-02 17:04:28 -0500

    Create the controllers and routes for the suppliers and categories tables

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -32,4 +32,50 @@ const customers = (req, res) => {
     });
 };
 
-module.exports = { customers };
+const suppliers = (req, res) => {
+  const rows = [];
+  fs.createReadStream(req.file.path)
+    .pipe(parse({ columns: true, trim: true }))
+    .on("data", (row) => rows.push(row))
+    .on("end", async () => {
+      try {
+        if (rows.length) {
+          const values = rows
+            .map((r) => `('${r.name}', '${r.email}')`)
+            .join(",");
+          sql.query(`INSERT INTO suppliers (name, email) VALUES ${values}`);
+          await log.create({
+            name: `Upload files to suppliers table, total rows added: ${rows.length}`,
+          });
+          res.status(200).json({ Uploded: true, total: rows.length });
+        }
+      } catch (error) {
+        res.status(500).json({ Uploded: false, error: "Internal Error" });
+        console.log(error);
+      }
+    });
+};
+
+const categories = (req, res) => {
+  const rows = [];
+  fs.createReadStream(req.file.path)
+    .pipe(parse({ columns: true, trim: true }))
+    .on("data", (row) => rows.push(row))
+    .on("end", async () => {
+      try {
+        if (rows.length) {
+          const values = rows.map((r) => `('${r.name}')`).join(",");
+          sql.query(`INSERT INTO categories (name) VALUES ${values}`);
+          await log.create({
+            name: `Upload files to categories table, total rows added: ${rows.length}`,
+          });
+          res.status(200).json({ Uploded: true, total: rows.length });
+        }
+      } catch (error) {
+        res.status(500).json({ Uploded: false, error: "Internal Error" });
+        console.log(error);
+      }
+    });
+};
+
+module.exports = { customers, suppliers, categories };
--- routes/uploads.route.js
+++ routes/uploads.route.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const multer = require("multer");
-const { customers } = require("../controllers/uploads.controller");
+const {
+  customers,
+  suppliers,
+  categories,
+} = require("../controllers/uploads.controller");
 
 const router = express.Router();
 const upload = multer({ dest: "../uploads/" });
 
 router.post("/upload/customers", upload.single("file"), customers);
+router.post("/upload/suppliers", upload.single("file"), suppliers);
+router.post("/upload/categories", upload.single("file"), categories);
 
 module.exports = router;
